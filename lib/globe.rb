e7bc2d6a79bef5a1d43e7e2cee7822e979512f8d
Ellen Marie Dash <me@duckie.co>
2017-11-29 21:34:30 -0500

    lmfao wow

--- lib/globe.rb
+++ lib/globe.rb
@@ -1,6 +1,6 @@
 require "globe/version"
 
-# Reimplements global variables using class variables.
+# Reimplements global variables using a Hash.
 #
 # Mostly because global_variable_defined?, global_variable_get, and
 # global_variable_set are magnificent things.
@@ -8,47 +8,29 @@ require "globe/version"
 # There are convenience methods defined on the Kernel module,
 # like Ruby has for class and instance variables.
 module Globe
-  @@globals = {}
-
-  class << self
-    # Returns `true` if the given instance variable is defined in _obj_.
-    # String arguments are converted to symbols.
-    def defined?(name)
-      @@globals.keys.include?(name)
-    end
-
-    def set(name, value)
-      @@globals[name] = value
-    end
-
-    def get(name)
-      @@globals[name]
-    end
-  end
+  Variables = Hash.new
 end
 
 module Kernel
-  #
   def global_variable_defined?(name)
-    Globe.defined?(name)
+    Globe::Variables.has_key?(name)
   end
 
   def global_variable_get(name)
-    Globe.get(name)
+    Globe::Variables.fetch(name)
   end
 
   def global_variable_set(name, value)
-    Globe.set(name, value)
+    Globe::Variables.store(name, value)
   end
 
-  #
   def global(obj)
     if obj.is_a?(Hash)
       obj.each do |k, v|
-        Globe.set(k, v)
+        global_variable_set(k, v)
       end
     else
-      Globe.get(obj)
+      global_variable_get(obj)
     end
   end
 end
